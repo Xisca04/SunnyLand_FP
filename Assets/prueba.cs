03ffd8c2498625637809c227ca76fdf50a0a77ae
Xisca04 <114388909+Xisca04@users.noreply.github.com>
2024-01-09 20:10:47 +0100

    update

--- Assets/prueba.cs
+++ Assets/prueba.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class prueba : MonoBehaviour
 {
+    public GameObject titlePanel;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        titlePanel.SetActive(true);
     }
 
     // Update is called once per frame
@@ -19,5 +21,10 @@ public class prueba : MonoBehaviour
     public void Play()
     {
         Debug.Log($"play");
+    }
+
+    public void StartButton()
+    {
+        titlePanel.SetActive(false);
     }
 }
