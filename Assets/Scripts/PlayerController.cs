a7e32757db2c17207f50c9555fa8c2ec09dbde25
Xisca04 <114388909+Xisca04@users.noreply.github.com>
2024-01-12 18:54:33 +0100

    movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,18 @@ public class PlayerController : MonoBehaviour
 {
     // Controller of the player
 
-    [SerializeField] private float runSpeed = 1000f;
-   // public float jumpForce = 10f;
+    private float runSpeed = 450f;
+    public float jumpForce = 10f;
+
+    [SerializeField] private Transform groundController;
+    [SerializeField] private Vector2 dimensionBox;
+    [SerializeField] private bool isGround;
+    [SerializeField] private LayerMask allGround;
+
+    private bool canJump = false;
 
     private Rigidbody2D _rigidbody;
-    //private Animator _anim;
+    private Animator _anim;
 
     private float horizontalMov = 0f;
     private bool lookRight = true;
@@ -18,7 +25,7 @@ public class PlayerController : MonoBehaviour
 
     private void Awake()
     {
-       // _anim = GetComponent<Animator>();
+        _anim = GetComponent<Animator>();
     }
 
     private void Start()
@@ -29,15 +36,26 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         horizontalMov = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        _anim.SetFloat("Horizontal", Mathf.Abs(horizontalMov)); // se añade el valor absoluto para controlar mejor la animación
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            // Jump
+            canJump = true;
+        }
     }
 
     private void FixedUpdate()
     {
+        isGround = Physics2D.OverlapBox(groundController.position, dimensionBox, 0, allGround);
         // que se mueva el personaje
-        Movement(horizontalMov * Time.fixedDeltaTime);
+        Movement(horizontalMov * Time.fixedDeltaTime, jumpForce);
+
+        canJump = false;
     }
 
-    private void Movement(float move)
+    private void Movement(float move, float jump)
     {
         Vector2 velocity = new Vector2(move, _rigidbody.velocity.y);
         _rigidbody.velocity = velocity;
@@ -51,6 +69,12 @@ public class PlayerController : MonoBehaviour
         {
             // Girar personaje
             TurnAroundL();
+        }
+
+        if(isGround && Input.GetKeyDown(KeyCode.Space))
+        {
+            isGround = false;
+            _rigidbody.AddForce(new Vector2(0f, jumpForce));
         }
     }
 
@@ -68,5 +92,11 @@ public class PlayerController : MonoBehaviour
         Vector2 scale = transform.localScale;
         scale.x = 10;
         transform.localScale = scale;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(groundController.position, dimensionBox);
     }
 }
