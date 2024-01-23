e8c1da6b7429754dfcebdf7aa43b234df2103d72
Xisca04 <114388909+Xisca04@users.noreply.github.com>
2024-01-23 14:48:37 +0100

    chest_system

---
+++ Assets/Scripts/ChestSystem.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestSystem : MonoBehaviour
+{
+    // Chest system -> collision --> scene
+
+    // escenas
+    // panel abrir o no cofre
+    // animación apertura y cierre
+    // envío a las escenas
+    private Animator _anim;
+    private void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            _anim.SetBool("IsOpened", true);
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            _anim.SetBool("IsOpened", false);
+        }
+    }
+    /*
+    private void OnCollisionEnter(Collision otherCollider)
+    {
+        if (otherCollider.gameObject.CompareTag("Player"))
+        {
+            _anim.SetBool("IsOpened", true);
+        }
+        else
+        {
+            _anim.SetBool("IsOpened", false);
+        }
+    }*/
+}
