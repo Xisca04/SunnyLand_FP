a425c5d032ff38b418e5ff1c2f5c932f843ad7af
Xisca04 <114388909+Xisca04@users.noreply.github.com>
2024-01-24 17:45:00 +0100

    design_final_scene

--- Assets/Scripts/PU_CorrectPath.cs
+++ Assets/Scripts/PU_CorrectPath.cs
@@ -2,24 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PU_RightPath : MonoBehaviour
+public class PU_CorrectPath : MonoBehaviour
 {
     // Power Up that shows the path to the right chest
 
     // Colllision con el player --> mostrar un objeto
 
-    [SerializeField] private GameObject rightPath;
+    [SerializeField] private GameObject correctPath;
 
     private void Start()
     {
-        rightPath.SetActive(false);
+        correctPath.SetActive(false);
     }
 
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.O))
         {
-            rightPath.SetActive(true);
+            correctPath.SetActive(true);
         }
     }
 }
