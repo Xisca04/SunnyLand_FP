8441448205e4920d17805beaf7cd85f4c4b77186
Xisca04 <114388909+Xisca04@users.noreply.github.com>
2024-02-16 21:57:03 +0100

    sound_design

--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -5,4 +5,49 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     //sound manager
+    
+    public class SoundAudioClip
+    {
+        public Sound sound;
+        public AudioClip audioClip;
+    }
+
+    public SoundAudioClip[] soundAudioClipArray;
+
+    public enum Sound
+    {
+        Cherry,
+        Gem,
+        PU_Path,
+        PU_Countdown_On,
+        PU_Countdown_Off,
+    }
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = this.gameObject.AddComponent<AudioSource>();
+    }
+
+    public void PlaySound(Sound sound)
+    {
+        audioSource.PlayOneShot(GetAudioClipFromSound(sound));
+    }
+
+    private AudioClip GetAudioClipFromSound(Sound sound)
+    {
+        foreach (SoundAudioClip soundAudioClip in soundAudioClipArray)
+        {
+            if (soundAudioClip.sound == sound)
+            {
+                return soundAudioClip.audioClip;
+            }
+        }
+
+        Debug.LogError("Sound" + sound + "not found");
+        return null;
+    }
+
+
 }
