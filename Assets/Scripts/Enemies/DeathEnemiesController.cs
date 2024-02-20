08c2d42f7781092a6d7ac3e8e1167e2f4fb5cae6
Xisca04 <114388909+Xisca04@users.noreply.github.com>
2024-02-20 19:43:21 +0100

    damage_player_enemy_vfx

--- Assets/Scripts/Enemies/DeathEnemiesController.cs
+++ Assets/Scripts/Enemies/DeathEnemiesController.cs
@@ -18,7 +18,6 @@ public class DeathEnemiesController : MonoBehaviour
     private IEnumerator VFXDeath()
     {
         GetComponent<SpriteRenderer>().enabled = false; // Asi se puede ver el efecto visual
-        Destroy(gameObject.GetComponent<BoxCollider2D>()); // Destruye el collider para que el player no colisione con este hasta que se detrya todo el objeto
         gameObject.transform.GetChild(0).gameObject.SetActive(true); // Accede al hijo del objeto uq es el que tiene el efecto visual
         yield return new WaitForSeconds(0.5f);
         Destroy(this.gameObject); // Destruye ambos 
