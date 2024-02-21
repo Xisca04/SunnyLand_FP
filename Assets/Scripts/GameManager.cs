0e44c890df40b1438f32104733593081b233cf63
Xisca04 <114388909+Xisca04@users.noreply.github.com>
2024-02-21 20:03:54 +0100

    design_finalLevel

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,7 +12,7 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.R))
         {
             PlayerPrefs.DeleteAll();
         }
