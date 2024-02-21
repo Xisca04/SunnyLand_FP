505435eda4361a8848b6c6cc69ee27a24349d324
Xisca04 <114388909+Xisca04@users.noreply.github.com>
2024-02-21 17:47:42 +0100

    credits_scene

--- Assets/Scripts/CreditsManager.cs
+++ Assets/Scripts/CreditsManager.cs
@@ -1,22 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CreditsManager : MonoBehaviour
 {
     // panel winner --> tiempo --> creditos
 
     [SerializeField] private GameObject winnerPanel;
+    [SerializeField] private GameObject credits;
+    [SerializeField] private Button mainMenuButton;
 
     private void Start()
     {
+        mainMenuButton.onClick.AddListener(GoToMainMenu);
         winnerPanel.SetActive(true);
+        credits.SetActive(false);
+        mainMenuButton.gameObject.SetActive(false);
         StartCoroutine("WinnerPanel");
     }
 
     private IEnumerator WinnerPanel()
     {
-        yield return new WaitForSeconds(3.5f);
+        yield return new WaitForSeconds(4f);
         winnerPanel.SetActive(false);
+        credits.SetActive(true);
+        mainMenuButton.gameObject.SetActive(true);
     }
+
+    private void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
 }
