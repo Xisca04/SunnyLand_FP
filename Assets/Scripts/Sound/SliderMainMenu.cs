496e8ddbaf5b43a32d5684483a29b144a2d54db6
Xisca04 <114388909+Xisca04@users.noreply.github.com>
2024-02-24 12:40:34 +0100

    slider

---
+++ Assets/Scripts/Sound/SliderMainMenu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderMainMenu : MonoBehaviour
+{
+    // Slider set music of the background music of the different levels
+
+    [SerializeField] private Slider sliderBackgroundMusic;
+}
