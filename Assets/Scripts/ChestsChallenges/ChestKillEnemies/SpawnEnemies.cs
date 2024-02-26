fd87e0c6a3013a9e0e624c97186e795895468eb8
Xisca04 <114388909+Xisca04@users.noreply.github.com>
2024-02-26 13:38:37 +0100

    particlesSystem

--- Assets/Scripts/ChestsChallenges/ChestKillEnemies/SpawnEnemies.cs
+++ Assets/Scripts/ChestsChallenges/ChestKillEnemies/SpawnEnemies.cs
@@ -7,6 +7,7 @@ public class SpawnEnemies : MonoBehaviour
     // Spawn random enemies to kill the player --> counter death enemies --> 10 deaths --> win --> > 10 --> loose
 
     [SerializeField] private GameObject[] _enemies;
+    [SerializeField] private ParticleSystem _emberParticles;
     private int enemyIndex;  
 
     public float startDelay = 1.5f;  //Determina qué tiempo tiene el jugador
@@ -15,6 +16,8 @@ public class SpawnEnemies : MonoBehaviour
     private void Start() // Llama periódicamente a la función SpawnRnadomAnimal
     {
         InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterial);
+        _emberParticles.Play();
+        // cunaod gane stop particles
     }
 
     private void SpawnRandomAnimal() //Función que determina la aletoriedad de los tipos de animales que van apareciendo en una posición aleatoria
