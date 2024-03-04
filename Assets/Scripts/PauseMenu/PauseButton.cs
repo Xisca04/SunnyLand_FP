f2a0046a4d4190141ec7e669267131476b202e10
Xisca04 <114388909+Xisca04@users.noreply.github.com>
2024-03-04 12:55:58 +0100

    cambioPauseMenu

--- Assets/Scripts/PauseMenu/PauseButton.cs
+++ Assets/Scripts/PauseMenu/PauseButton.cs
@@ -8,13 +8,12 @@ public class PauseButton : MonoBehaviour
 
     private void Start()
     {
-        gameObject.transform.GetChild(1).gameObject.SetActive(false);
+        gameObject.transform.GetChild(0).gameObject.SetActive(false);
     }
 
     public void PauseMenuOn()
     {
-        gameObject.transform.GetChild(1).gameObject.SetActive(true);
+        gameObject.transform.GetChild(0).gameObject.SetActive(true);
         Time.timeScale = 0f;
-        // SoundManager.PlaySound(SoundManager.Sound.ButtonOver);
     }
 }
